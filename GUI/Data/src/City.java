91250f9ea3ef99b34c13cfb53b04492fcc9b3304
Jeffrey A <jalhassan@umassd.edu>
2017-03-18 18:10:25 -0400

    update on City.java

--- GUI/Data/src/City.java
+++ GUI/Data/src/City.java
@@ -1,13 +1,13 @@
 import java.math.BigDecimal;
 
 public class City {
-	
+
 	private int id;
 	private int population;
 	private String name;
 	private String cc;
 	private String district;
-	
+
 	public City(int id, String name, String cc, String district, int population)
 	{
 		this.id = id;
@@ -16,41 +16,37 @@ public class City {
 		this.district = district;
 		this.population = population;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 *  Methods for call
 	 */
-	
+
 	public int getID(){
 		return id;
 	}
-	
+
 	public int getPopulation(){
 		return population;
 	}
-	
+
 	public String getName(){
 		return name;
 	}
-	
+
 	public String getCC(){
 		return cc;
 	}
-	
+
 	public String getDistrict(){
 		return district;
 	}
-	
+
 	// toString method
 	public String toString(){
-		return String.format("City [id=%s, name=%s, country-code=%s, district=%s]",
+		return String.format("City [ID=%s, name=%s, CountryCode=%s, District=%s]",
 							id, name, cc, district);
 	}
-	
+
 
 }
-
-
-
-
