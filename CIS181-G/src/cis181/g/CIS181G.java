2bfec6538ee496aae78e9d4872627eb12b953e3e
Jeffrey A <alhassan.jeffrey@yahoo.com>
2017-04-28 21:49:50 -0400

    create cis test

---
+++ CIS181-G/src/cis181/g/CIS181G.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cis181.g;
+
+/**
+ *
+ * @author Jeffrey A
+ */
+public class CIS181G {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
